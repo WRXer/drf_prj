8e3ca16f14a5009c3ca9050a3933d79855994186
WRXer <king_311@mail.ru>
2023-07-12 17:49:05 +0400

    описал КРУД для моделей лессон через Генерик-классы

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,21 @@
+from django.urls import path
+
 from main.apps import MainConfig
 from rest_framework.routers import DefaultRouter
 
-from main.views import CourseViewSet
+from main.views import CourseViewSet, LessonCreateAPIView, LessonRetrieveAPIView, LessonListAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MainConfig.name
 
 
 router = DefaultRouter()
-router.register('course', CourseViewSet, basename='course')
+router.register(r'course', CourseViewSet, basename='course')
 
 urlpatterns = [
-
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 ] + router.urls
